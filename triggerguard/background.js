a99eb7f131be0bf35a303fe6ebe622f8c59a60ea
Alex Herdzik <ajh2510@rit.edu>
2014-12-21 15:03:35 -0800

    Rearranged files

--- triggerguard/background.js
+++ triggerguard/background.js
@@ -2,14 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Simple extension to replace lolcat images from
-// http://icanhascheezburger.com/ with loldog images instead.
+/**
+ * @filedescription Initializes the extension's background page.
+ */
 
+var trigger = new TriggerGuard();
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
-    console.log("Cat intercepted: " + info.url);
-    // Redirect the lolcal request to a random loldog URL.
-    var i = Math.round(Math.random() * loldogs.length);
     return {redirectUrl: "https://31.media.tumblr.com/tumblr_m8pwmcn9wy1qbbiyo.jpg"};
   },
   // filters
@@ -20,4 +19,5 @@ chrome.webRequest.onBeforeRequest.addListener(
     types: ["image"]
   },
   // extraInfoSpec
-  ["blocking"]);
+  ["blocking"]
+  );
